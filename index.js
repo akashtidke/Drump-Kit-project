0e3533e0400e2eac117a7a72ac63574eb5e92277
akash <akashtidke2014@gmail.com>
2022-03-05 01:05:52 +0530

    add

---
+++ index.js
@@ -0,0 +1,57 @@
+
+var btn=document.querySelectorAll(".drum").length;
+
+for(var i=0;i<btn;i++){
+
+document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+   var btnIneer=this.innerHTML;
+   makeSound(btnIneer);
+  
+
+    this.style.color="black";
+    
+    
+   
+   
+
+    
+})
+
+}
+document.addEventListener("keypress",function(event){
+   makeSound(event.key);
+})
+function makeSound(key){
+    switch(key){
+        case "w":
+          var audio = new Audio('crash.mp3');
+             audio.play();
+             break;
+           
+           case "a":
+          var audio = new Audio('kick-bass.mp3');
+             audio.play();
+           break;
+           case "s":
+          var audio = new Audio('snare.mp3');
+             audio.play();
+           break;
+           case "d":
+          var audio = new Audio('tom1.mp3');
+             audio.play();
+           break;
+           case "j":
+          var audio = new Audio('tom2.mp3');
+             audio.play();
+           break;
+           case "k":
+          var audio = new Audio('tom3.mp3');
+             audio.play();
+           break;
+           case "l":
+            var audio = new Audio('tom4.mp3');
+               audio.play();
+               break;
+           }
+
+}
